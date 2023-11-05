f752f2ae372a1ebdf90bd95d925c680f44915ad3
rutuja-1201 <rutujadhore60@gmail.com>
2023-11-05 09:20:54 +0530

    chsnges in route

--- src/routes/authroutes.js
+++ src/routes/authroutes.js
@@ -1,8 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const User = require('../models/user');
 const controller =require('../controllers/authcontroller')
 
 
